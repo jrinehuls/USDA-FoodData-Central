94fd3cda0ebe9f1acc2e597bb4bc371a1c814161
Justin Rinehuls <jrinehuls@gmail.com>
2023-06-14 20:34:47 -0400

    initial commit

---
+++ src/main/java/com/jrinehuls/usda/UsdaApplication.java
@@ -0,0 +1,13 @@
+package com.jrinehuls.usda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrinehuls/usda/UsdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrinehuls.usda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
