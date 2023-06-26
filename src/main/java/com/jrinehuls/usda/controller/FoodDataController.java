6b9e82380ac4804d4434594bdcb867d01c03c476
Justin Rinehuls <jrinehuls@gmail.com>
2023-06-25 21:26:48 -0400

    Hide API Key. Separate controllers

--- src/main/java/com/jrinehuls/usda/controller/FoodDataController.java
+++ src/main/java/com/jrinehuls/usda/controller/FoodDataController.java
@@ -1,13 +1,16 @@
 package com.jrinehuls.usda.controller;
 
 import com.jrinehuls.usda.model.Food;
-import com.jrinehuls.usda.model.ToDo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.Map;
 
 @RestController
 public class FoodDataController {
@@ -17,16 +20,17 @@ public class FoodDataController {
 
 
     @GetMapping("food/{fdcId}")
-    public ResponseEntity<Food> getFood(@PathVariable("fdcId") Long id) {
-        String theUrl = "https://api.nal.usda.gov/fdc/v1/food/" + id;
+    public ResponseEntity<?> getFood(@PathVariable("fdcId") Long id, @RequestHeader(name = "x-api-key") String token) {
+
+        String url = "https://api.nal.usda.gov/fdc/v1/food/" + id;
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("x-api-key", "UVi63ywl7bpzs0NNXkF6ElTikJNlhXWXvq9zgL9N");
+        headers.set("x-api-key", token);
 
-        HttpEntity<Food> entity = new HttpEntity<>(headers);
+        HttpEntity<Food> requestEntity = new HttpEntity<>(headers);
 
-        ResponseEntity<Food> response = restTemplate.exchange(theUrl, HttpMethod.GET, entity, Food.class);
-        return new ResponseEntity<Food>(response.getBody(), HttpStatus.OK);
+        Food response = restTemplate.exchange(url, HttpMethod.GET, requestEntity, Food.class).getBody();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/jrinehuls/usda/model/Food.java
+++ src/main/java/com/jrinehuls/usda/model/Food.java
@@ -3,6 +3,13 @@ package com.jrinehuls.usda.model;
 public class Food {
 
     private Long fdcId;
+    private String footnote;
+    private String description;
+    private String publicationDate;
+
+    public Food() {
+
+    }
 
     public Long getFdcId() {
         return fdcId;
@@ -12,4 +19,27 @@ public class Food {
         this.fdcId = fdcId;
     }
 
+    public String getFootnote() {
+        return footnote;
+    }
+
+    public void setFootnote(String footnote) {
+        this.footnote = footnote;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 }
